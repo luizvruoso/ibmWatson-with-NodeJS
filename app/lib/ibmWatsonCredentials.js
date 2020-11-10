00345f86881d54aa6a1dabc1209a0a39029658d6
luizvruoso <53343378+luizvruoso@users.noreply.github.com>
2020-11-10 00:11:14 -0300

    Finalizada integração

--- app/lib/ibmWatsonCredentials.js
+++ app/lib/ibmWatsonCredentials.js
@@ -1,11 +1,10 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
 const {IamAuthenticator} = require('ibm-watson/auth');
 
-const assistant = new AssistantV1({
+module.exports.assistant = new AssistantV1({
     url: 'https://gateway.watsonplatform.net/assistant/api',
     version: '2020-01-04',
     authenticator: new IamAuthenticator ({ apikey:'API' })
 
 });
 
-module.export = {assistant};
